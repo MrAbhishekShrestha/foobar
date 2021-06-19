8dd7e3a8ce721bce181b4f78ecb46fed7cdc5238
MrAbhishekShrestha <abhishek.shr2000@gmail.com>
2021-06-19 12:55:48 +0545

    remove old incorrect attmept

--- level1/solution.py
+++ level1/solution.py
@@ -1,28 +1,6 @@
-# level 1 
-def solution0(s):
-    # your code here
-    n = len(s)
-    cuts = 0 
-    isEven = (n % 2 == 0)
-    while isEven:
-        isValid = True
-        count = 1
-        m = n // 2**count
-        testString = s[:m]
-        index = m  
-        for _ in range(2**count-1):
-            checkString = s[index:index + m]
-            if testString != checkString:
-                isValid = False
-                break
-            index = index + m
-        isEven = isValid
-        if isEven:
-            count += 1
-            cuts += 1
-            isEven = len(testString) % 2 == 0
-    return 2**cuts
-
+"""
+Level 1
+"""
 def solution1(s):
     """
     Algorithm: 
