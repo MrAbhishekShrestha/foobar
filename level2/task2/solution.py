2835d8ae74f0c347b74e2c413d9fbe84d2c45667
MrAbhishekShrestha <abhishek.shr2000@gmail.com>
2021-06-20 17:23:38 +0545

    add initial files for level 2 task 2

---
+++ level2/task2/solution.py
@@ -0,0 +1,16 @@
+"""
+Level 2 - Task 2 
+Elevator Maintenance
+Google Foobar 
+
+date: 20/06/2021
+"""
+def solution(l):
+    # your code here 
+    x = 1
+
+def main():
+    x = 1
+
+if __name__ == "__main__":
+    main()
