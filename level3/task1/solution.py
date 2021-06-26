e5d64160d43f253d06b7b28cba7c6bf3ad6342ef
MrAbhishekShrestha <abhishek.shr2000@gmail.com>
2021-06-26 10:34:40 +0545

    Add initial files for level3 task1

---
+++ level3/task1/solution.py
@@ -0,0 +1,17 @@
+"""
+Google Foobar 
+Challenge 3 
+Task 1 
+
+date: 26th June 2021
+"""
+
+def solution(map):
+    # your code here 
+    pass 
+
+def main():
+    pass 
+
+if __name__ == "__main__":
+    main()
