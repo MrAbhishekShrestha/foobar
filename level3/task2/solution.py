ab01fd8c891fb3b074e2d3c4540bd44f801040ba
MrAbhishekShrestha <abhishek.shr2000@gmail.com>
2021-07-08 12:34:28 +0545

    bomb baby solution

--- level3/task2/solution.py
+++ level3/task2/solution.py
@@ -1,9 +1,28 @@
+"""
+Google Foobar 
+Level 3 task 2 
+Bomb Baby
+"""
 def solution(x, y):
     # your code here 
-    pass 
+    m, f = int(x), int(y)
+    generation = 0 
+
+    while (m != 1 and f != 1):
+        if m < 1 or f < 1: 
+            return "impossible"
+        if max(m, f) % min(m, f) == 0:
+            return "impossible"
+        else: 
+            generation += max(m, f)//min(m, f)
+            m, f =  max(m, f)%min(m, f), min(m, f)
+    return str(generation + max(m, f) - 1)
 
 def main():
-    pass 
+    # sample test cases 
+    print(f"expected:1 |   got:{solution('2', '1')}")
+    print(f"expected:4 |   got:{solution('4', '7')}")
+    print(f"expected:impossible |   got:{solution('2', '4')}")
 
 if __name__ == "__main__":
     main()
